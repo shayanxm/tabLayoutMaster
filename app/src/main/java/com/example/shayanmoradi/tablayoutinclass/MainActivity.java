705a25ed0222ac2c1650a7ad2c25e85c0f0a75b1
shayanxm <shayan.mwp@gmail.com>
2018-12-26 08:02:04 +0330

    Initial commit

---
+++ app/src/main/java/com/example/shayanmoradi/tablayoutinclass/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.shayanmoradi.tablayoutinclass;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.streamline_18_512,
+            R.drawable.streamline_18_512,
+            R.drawable.streamline_18_512
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        adapter = new TabAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment(), "Tab 1");
+        adapter.addFragment(new fragment2(), "Tab 2");
+        adapter.addFragment(new Tab1Fragment(), "Tab 3");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+    }
+
+}
